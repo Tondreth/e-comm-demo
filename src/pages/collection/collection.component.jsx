4cfc4644e460a0b2e457a1dec01ad46d3d3ea774
Nikolay Nikolov <nikolay.veselinov.nikolov@gmail.com>
2021-04-13 12:41:56 +0300

    add redux to shop

---
+++ src/pages/collection/collection.component.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import './collection.styles.scss'
+import CollectionItem from '../../components/collection-item/collection-item.component'
+
+const CollectionPage = ({match}) => (
+    <div className='collection-page'>
+        <h2>CATEGORY PAGE</h2>
+    </div>
+)
+
+export default CollectionPage
